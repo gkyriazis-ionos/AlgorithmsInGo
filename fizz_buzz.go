5b834b0edc4609dafeedac6d99cd864ba5877da1
Georgios Leandros Kyriazis <georgios.kyriazis@ionos.com>
2022-07-20 14:25:55 +0200

    added fizz buzz and updated the reverse test for Japanese chars

---
+++ fizz_buzz.go
@@ -0,0 +1,36 @@
+package AlgorithmsWithGo
+
+import "fmt"
+
+// FizzBuzz will print out all of the numbers
+// from 1 to N replacing any divisible by 3
+// with "Fizz", and divisible by 5 with "Buzz",
+// and any divisible by both with "Fizz Buzz".
+//
+// Note: The test for this is a little
+// complicated so that you can just use the
+// `fmt` package and print to standard out.
+// I wouldn't normally recommend this, but did
+// it here to make life easier for beginners.
+
+func FizzBuzz(n int) {
+	for i := 1; i < n; i++ {
+		printFizzBuzzValue(i)
+		fmt.Print(", ")
+	}
+	printFizzBuzzValue(n)
+	fmt.Println()
+}
+
+func printFizzBuzzValue(n int) {
+	// I could also use switch
+	if n%5 == 0 && n%3 == 0 {
+		fmt.Print("Fizz Buzz")
+	} else if n%5 == 0 {
+		fmt.Print("Buzz")
+	} else if n%3 == 0 {
+		fmt.Print("Fizz")
+	} else {
+		fmt.Print(n)
+	}
+}
