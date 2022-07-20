a9763a90c3eedb8680f483a4a98f96ec0254be06
Georgios Leandros Kyriazis <georgios.kyriazis@ionos.com>
2022-07-20 16:24:53 +0200

    added dec to base

---
+++ dec_to_base.go
@@ -0,0 +1,24 @@
+package AlgorithmsWithGo
+
+import (
+	"strconv"
+)
+
+func DecToBase(dec, base int) string {
+	output := ""
+	for dec != 0 {
+		digit := ""
+		switch {
+		case dec%base >= 10:
+			a := dec % base
+			b := a % 10
+			c := int('A') + b
+			digit = string(rune(c))
+		default:
+			digit = strconv.Itoa(dec % base)
+		}
+		output = digit + output
+		dec = dec / base
+	}
+	return output
+}
