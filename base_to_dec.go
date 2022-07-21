78447be1484bf4cc82970ce844f6579a83a000d4
Georgios Leandros Kyriazis <georgios.kyriazis@ionos.com>
2022-07-21 10:41:59 +0200

    added base_to_dec

--- base_to_dec.go
+++ base_to_dec.go
@@ -9,16 +9,17 @@ import (
 func BaseToDec(value string, base int) int {
 	power := 0
 	num := 0
+	hexToDec := map[string]string{
+		"A": "10",
+		"B": "11",
+		"C": "12",
+		"D": "13",
+		"E": "14",
+		"F": "15",
+	}
 	digits := strings.Split(value, "")
 	for i := len(digits) - 1; i >= 0; i-- {
-		hexToDec := map[string]string{
-			"A": "10",
-			"B": "11",
-			"C": "12",
-			"D": "13",
-			"E": "14",
-			"F": "15",
-		}
+
 		val, ok := hexToDec[digits[i]]
 		d := 0
 		if ok {
