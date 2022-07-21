b4ba4722dd1b697fa0d1fc52d5e6e4e7764203d1
Georgios Leandros Kyriazis <georgios.kyriazis@ionos.com>
2022-07-21 14:35:27 +0200

    added sorts

---
+++ bubble_sort.go
@@ -0,0 +1,37 @@
+package AlgorithmsWithGo
+
+import "sort"
+
+// BubbleSortInt will sort a list of integers using the bubble sort algorithm.
+//
+// Big O: O(N^2), where N is the size of the list
+func BubbleSortInt(list []int) {
+	for i := 0; i < len(list); i++ {
+		for j := 0; j < len(list)-1-i; j++ {
+			if list[j] > list[j+1] {
+				list[j], list[j+1] = list[j+1], list[j]
+			}
+		}
+	}
+}
+
+// BubbleSortString is a bubble sort for string slices. Try implementing it for
+// practice.
+func BubbleSortString(list []string) {
+}
+
+type Person struct {
+	firstName string
+	lastName  string
+	age       int
+}
+
+// BubbleSortPerson uses bubble sort to sort Person slices by: Age, then
+// LastName, then FirstName. Try implementing it for practice.
+func BubbleSortPerson(people []Person) {
+}
+
+// BubbleSort uses the standard library's sort.Interface to sort. Try
+// implementing it for practice.
+func BubbleSort(list sort.Interface) {
+}
